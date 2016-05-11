c6addb62a1fda02eed97bd49320018aa8898b045
syshen <sysheen@gmail.com>
2016-05-11 22:35:32 +0800

    adding Mac target

---
+++ StopwordsOSX/StopwordsOSX.h
@@ -0,0 +1,19 @@
+//
+//  StopwordsOSX.h
+//  StopwordsOSX
+//
+//  Created by Shih Yun Shen on 5/11/16.
+//  Copyright © 2016 Shih Yun Shen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StopwordsOSX.
+FOUNDATION_EXPORT double StopwordsOSXVersionNumber;
+
+//! Project version string for StopwordsOSX.
+FOUNDATION_EXPORT const unsigned char StopwordsOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StopwordsOSX/PublicHeader.h>
+
+
