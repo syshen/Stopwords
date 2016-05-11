c9825bcf0055480b7598eefcd4395e7c849c9708
syshen <sysheen@gmail.com>
2016-05-11 21:14:07 +0800

    Initial Commit

---
+++ Stopwords/Stopwords.h
@@ -0,0 +1,19 @@
+//
+//  Stopwords.h
+//  Stopwords
+//
+//  Created by Shih Yun Shen on 5/11/16.
+//  Copyright © 2016 Shih Yun Shen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stopwords.
+FOUNDATION_EXPORT double StopwordsVersionNumber;
+
+//! Project version string for Stopwords.
+FOUNDATION_EXPORT const unsigned char StopwordsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stopwords/PublicHeader.h>
+
+
